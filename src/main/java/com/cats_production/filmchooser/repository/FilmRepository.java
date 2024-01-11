7f905e20874aee571caf2d090025cb1113757b5d
Nadzeya <primisen17@gmail.com>
2024-01-11 13:03:59 +0100

    Fixin film id in  Repository

--- src/main/java/com/cats_production/filmchooser/repository/FilmRepository.java
+++ src/main/java/com/cats_production/filmchooser/repository/FilmRepository.java
@@ -4,6 +4,8 @@ import com.cats_production.filmchooser.domain.Film;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface FilmRepository extends CrudRepository<Film, Long> {
+public interface FilmRepository extends CrudRepository<Film, UUID> {
 }
