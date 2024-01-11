69bd11ff60482ab8bb1aecbcbe63a35e6eb2b39f
Nadzeya <primisen17@gmail.com>
2024-01-11 12:45:22 +0100

    Added header to response for adding new film request

--- src/main/java/com/cats_production/filmchooser/controller/FilmController.java
+++ src/main/java/com/cats_production/filmchooser/controller/FilmController.java
@@ -4,6 +4,7 @@ import com.cats_production.filmchooser.domain.Film;
 import com.cats_production.filmchooser.service.FilmService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,8 +32,11 @@ public class FilmController {
 
     @PostMapping
     public ResponseEntity<String> addFilm(@RequestBody Film film) {
-        filmService.add(film);
+        Film savedFilm = filmService.add(film);
 
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/films" + savedFilm.getId().toString());
+
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
     }
 }
