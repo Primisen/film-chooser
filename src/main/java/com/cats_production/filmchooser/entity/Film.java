c0bafc12e3d034bf612556a0a30b32f7323bc5a3
Nadzeya <primisen17@gmail.com>
2024-01-17 10:38:38 +0100

    Updated film entity

--- src/main/java/com/cats_production/filmchooser/entity/Film.java
+++ src/main/java/com/cats_production/filmchooser/entity/Film.java
@@ -1,13 +1,14 @@
 package com.cats_production.filmchooser.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.util.Date;
 import java.util.UUID;
@@ -20,8 +21,11 @@ import java.util.UUID;
 public class Film {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
     private UUID id;
+
     private String name;
     private Date year;
 }
